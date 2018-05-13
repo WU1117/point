5bd6c66c4abd631ddc989ef9d82df030955a45e5
WU1117 <657753588@qq.com>
2018-05-13 23:19:25 +0800

    粒子变换

---
+++ assert/car.js
@@ -0,0 +1,23 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "car.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 8476,
+        "faces"         : 14691,
+        "normals"       : 9460,
+        "uvs"           : 8585,
+        "materials"     : 0
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "buffers": "car.bin"
+
+}
---
+++ assert/people.js
@@ -0,0 +1,23 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "people.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 5746,
+        "faces"         : 8548,
+        "normals"       : 12632,
+        "uvs"           : 3097,
+        "materials"     : 0
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "buffers": "people.bin"
+
+}
